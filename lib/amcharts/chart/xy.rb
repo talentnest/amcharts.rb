35dcc6f6558044fe6a43ccf294a860482b4e51a3
Robert Vracaric <rvracaric@selfmgmt.com>
2017-05-04 15:20:01 -0400

    Fix amchart_type for XY charts

--- lib/amcharts/chart/xy.rb
+++ lib/amcharts/chart/xy.rb
@@ -3,5 +3,9 @@ require 'amcharts/chart'
 module AmCharts
   class Chart::XY < Chart::Rectangular
     # Stub class for XY charts
+
+    def self.amchart_type
+      'AmCharts.AmXYChart()'
+    end
   end
 end
